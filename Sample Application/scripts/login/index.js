a43bd2ebb24be4a6cd7440aee91180df3ef31614
Jorge Castro <jorge.castro.au@gmail.com>
2018-05-07 15:19:17 -0500

    Update index.js

--- Sample Application/scripts/login/index.js
+++ Sample Application/scripts/login/index.js
@@ -4,13 +4,13 @@ const Amplify = require("aws-amplify");
 Amplify.default.configure({
     Auth: {
         // REQUIRED - Amazon Cognito Identity Pool ID
-        identityPoolId: 'us-east-2:188df8dd-a1c4-4622-857f-0008b9c59da5',
+        identityPoolId: '{YOUR ID}',
         // REQUIRED - Amazon Cognito Region
-        region: 'us-east-2',
+        region: '{YOUR REGION}',
         // OPTIONAL - Amazon Cognito User Pool ID
-        userPoolId: 'us-east-2_I4XXj25Wt',
+        userPoolId: '{YOUR ID}',
         // OPTIONAL - Amazon Cognito Web Client ID
-        userPoolWebClientId: '27f8ob7fqv7fhfs5u4issu2neq',
+        userPoolWebClientId: '{YOUR KEY}',
         // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
         mandatorySignIn: false,
     }
